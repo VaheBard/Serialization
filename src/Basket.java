8ef8f13a043881f3168d4109d8c7355b1ed7b6d3
Vahe <vabardeg@gmail.com>
2023-03-24 03:23:38 +0400

    commit

--- src/Basket.java
+++ src/Basket.java
@@ -1,86 +1,52 @@
 import java.io.*;
 import java.util.Arrays;
 
-
 public class Basket implements Serializable {
-    private static String[] products;
-    private static int[] prices;
-    private static int[] totalBasket;
-    private static int sum;
+    private static final long serialVersionUID = 1L;
+    private String[] products;
+    private int[] prices;
+    private int[] totalBasket;
+    private int sum;
 
-    public Basket(String[] productsBasket, int[] pricesBasket) {
-//конструктор, принимающий массив цен и названий продуктов;
-        this.products = productsBasket;
+    public Basket(String[] products, int[] pricesBasket) {
+        this.products = products;
         this.prices = pricesBasket;
         this.totalBasket = new int[products.length];
         this.sum = 0;
     }
 
     public void addToCart(int productNum, int amount) {
-//метод добавления amount штук продукта номер productNum в корзину
-        int currentPrice = prices[productNum];  //цена этого продукта
+        int currentPrice = prices[productNum];  //цена выбранного продукта
         totalBasket[productNum] += amount;
         sum += currentPrice * amount;
     }
 
     public void printCart() {
-//метод вывода на экран покупательской корзины.
         for (int i = 0; i < totalBasket.length; i++) {
             if (!(totalBasket[i] == 0)) {
-                System.out.printf("%s %d шт. по %d руб./шт. - %d руб в сумме; \n",
-                        products[i], totalBasket[i], prices[i],
-                        (totalBasket[i] * prices[i]));
+                System.out.println(products[i] + " " + totalBasket[i] + " шт, по цене " +
+                        prices[i] + " руб/шт " + totalBasket[i] * prices[i] +
+                        " руб в сумме");
             }
         }
-        System.out.printf("Итого: %d руб. \n", sum);
     }
 
-    public void saveTxt(File textFile) {
-        try (OutputStream os = new FileOutputStream(textFile)) {
-            for (String product : products)
-                os.write((product + " ").getBytes());
-            os.write("\n".getBytes());
-            for (int price : prices)
-                os.write((price + " ").getBytes());
-            os.write("\n".getBytes());
-            for (int amount : totalBasket)
-                os.write((amount + " ").getBytes());
-            os.write(("\n" + sum).getBytes());
-            os.flush();
-        } catch (IOException ex) {
+    public void saveBin(File file) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+            oos.writeObject(this);
+            oos.flush();
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
     }
 
-    public static Basket loadFromTxtFile(File textFile) {
-        try (
-                BufferedReader buf = new BufferedReader(new FileReader(textFile))
-        ) {
-
-            String[] strProd = buf.readLine().split(" ");
-            for (int i = 0; i < products.length; i++) {
-                products[i] = strProd[i];
-            }
-            String[] priceStr = buf.readLine().split(" ");
-            for (int i = 0; i < prices.length; i++) {
-                prices[i] = Integer.parseInt(priceStr[i]);
-            }
-
-            String[] strPrice = buf.readLine().split(" ");
-            for (int i = 0; i < totalBasket.length; i++) {
-                totalBasket[i] = Integer.parseInt(strPrice[i]);
-            }
-            String strSum = buf.readLine();
-            sum = Integer.parseInt(strSum);
-
-        } catch (IOException ex) {
+    public static Basket loadFromBinFile(File file) {
+        Basket basket = null;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            basket = (Basket) ois.readObject();
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
-        return null;
+        return basket;
     }
-
-    public int getSum() {
-        return sum;
-    }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) throws IOException {
+        File fileBinary = new File("basket.bin");
+        int[] prices = {50, 14, 80};
+        String[] products = {"Молоко", "Хлеб", "Гречка"};
+        Scanner scanner = new Scanner(System.in);
         /*пользователь вводит номер продукта, тобиш выбирает продукт, затем пробел и указыат количество
         * выбранного пробукта, так до тех пор пока не введет "end", после чего программа выводит итог
         * например : Молоко 10 шт 50 руб/шт 500 руб в сумме
                      Гречневая крупа 1 шт 80 руб/шт 80 руб в сумме
                      Итого 580 руб*/
-        String[] products = {"Молоко", "Хлеб", "Гречка"};
-
-        int[] prices = {50, 14, 80};
         System.out.println("Список возможных товаров для покупки");
 
 //выводим на экран информацию о продуктах и их цен с помощью цыкла for
@@ -20,25 +22,20 @@ public class Main {
             System.out.println((i + 1) + ". " + products[i] + " " + prices[i] + " руб/шт");
         }
 
-        File file = new File("basket.txt");
-
-        Scanner scanner = new Scanner(System.in);
-
-        Basket basket = new Basket(products, prices);
+        Basket basket;
         /*Проверяем существование файла, если его нет, то все начинается с нуля, иначе загружается существующая корзина
         и следующие покупки добавляются на нее
          */
-        if (file.exists()) {
-            Basket.loadFromTxtFile(file);
+        if (fileBinary.exists()) {
+            basket = Basket.loadFromBinFile(fileBinary);
 
             basket.printCart();
 
         } else {
-            file.createNewFile();
+            basket = new Basket(products, prices);
 
-            System.out.print("Покупка начинается с пустой корзины! ");
+            System.out.print("Корзина пустая! ");
         }
-        int total = 0;
 
         while (true) {
 
@@ -48,21 +45,20 @@ public class Main {
             String enter = scanner.nextLine();
 
             if ("end".equals(enter)) {
-                System.out.println("Вы завершили покупки, к оплате " + basket.getSum() + " руб\n");
+                System.out.println("Вы завершили покупки\n");
                 break;
             }
             String[] parts = enter.split(" ");
 
             int productNumber = Integer.parseInt(parts[0]) - 1;
             int productCount = Integer.parseInt(parts[1]);
-            total += prices[productNumber] * productCount;
+
             basket.addToCart(productNumber, productCount);
-            basket.saveTxt(file);
-
-
+            basket.saveBin(fileBinary);
             System.out.println("Ваша корзина:");
             basket.printCart();
         }
+
     }
 }
 
